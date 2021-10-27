8ea768f3e10c0e0f722e911df088d7d5a6b682dc
Suvam Routray <72883939+RayOPnub@users.noreply.github.com>
2021-10-27 12:05:35 +0530

    sum of n numbers

---
+++ sumofnNaturalNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+    using namespace std;
+    int main()
+    {
+        
+       int s , a; 
+        cout << “Enter the n numbers : “; 
+        //user input 
+        cin >> a; 
+        //calculating 
+        sum s= a*(a+1)/2; 
+        cout<<“The Sum of first “<< a <<” Natural Numbers is “<< s; 
+        return 0;
+    }
